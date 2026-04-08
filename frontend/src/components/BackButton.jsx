4710bb0e74e69311d608ff1e5ca5feaec4de499c
Echefu Tobechukwu <tobex360@gmail.com>
2026-04-08 13:20:21 +0100

    feat: Add BackButton component for navigation
    - Create a reusable BackButton component using Ant Design
    - Component navigates to the previous page using React Router's useNavigate

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
+function BackButton({ text = '', style = {}, ...props }) {
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      icon={<ArrowLeftOutlined />}
+      shape='circle'
+      onClick={() => navigate(-1)}
+      style={{ marginBottom: '16px', ...style }}
+      {...props}
+    >
+      {text}
+    </Button>
+  );
+}
+
+export default BackButton;
